b9e975498bd74bc0db50f371befdfe0b75917175
Maria Eduarda Borges <134969447+mariaeduardaborges@users.noreply.github.com>
2024-06-20 20:56:15 -0300

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,17 @@
+// Navbar.js
+
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li><a href="#">Página Inicial</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Calendário</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
